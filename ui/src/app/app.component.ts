f568edfbffafb788e240b63365ff0643bf3f7e67
gayana <gayan.kavindra06@gmail.com>
2017-11-11 23:46:26 +0530

    Move service class to the app root folder.

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AppService } from './services/app.service';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { AppService } from './services/app.service';
+import { AppService } from './app.service';
 import { HttpClientModule } from '@angular/common/http';
 
 
