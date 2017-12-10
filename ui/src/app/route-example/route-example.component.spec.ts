ec1d860cb3a8ab2b51fb5f24b68a3504840a2cb5
gayana <gayan.kavindra06@gmail.com>
2017-12-10 17:38:41 +0530

    Fix frontend unit tests

--- ui/src/app/route-example/route-example.component.spec.ts
+++ ui/src/app/route-example/route-example.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { RouteExampleComponent } from './route-example.component';
 
@@ -8,9 +9,12 @@ describe('RouteExampleComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RouteExampleComponent ]
+      declarations: [ RouteExampleComponent ],
+      imports: [
+        RouterTestingModule
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
